a5e58c3760eb126a7bb7ffa3eee928f86bff7189
Jai Khanna <jka@capricorn.de>
2017-07-31 16:00:49 +0200

    add crates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate rand;
+extern crate rusty_machine;
+
+use rand::Rng;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    let secret_number=rand::thread_rng().gen_range(1,101);
+    println!("The secret number is {}",secret_number);
+    println!("Enter your guess. ");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    println!("You guessed: {}", guess);
 }
