f0bbc789687959441815c599d5cae91e728d3cc0
Jai Khanna <jka@capricorn.de>
2017-08-01 16:08:27 +0200

    update main

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate rusty_machine;
 
 use rand::Rng;
 use std::io;
+use std::cmp::Ordering
 
 fn main() {
     println!("Guess the number!");
@@ -12,4 +13,12 @@ fn main() {
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Failed to read line");
     println!("You guessed: {}", guess);
+    match guess.cmp(&secret_number)
+    {
+        Ordering::Less => println!("Too small!")
+        Ordering::Greater => println!("Too big!")
+        Ordering::Equal => println!("Correct guess! You win")
+    }
+    
+    
 }
